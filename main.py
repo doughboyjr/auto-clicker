52e6cd68dd715df30941a6930f9bf62cb26a1205
giovanni <giovanni.cecilia@outlook.com>
2022-12-22 19:20:59 -0500

    added imports

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import time
 import threading
+from pynput.mouse import Controller, Button
+from pynput.keyboard import Listener, KeyCode
+
+TOGGLE_KEY = KeyCode(char="t")
+
+Clicking = False
+mouse = Controller()
+
+
+
 
